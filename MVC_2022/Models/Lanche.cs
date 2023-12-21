47f2d9a486e11353bb3d2d5b84e81a173d74c8d7
marciocharrone <marciocharrone@hotmail.com>
2023-12-21 12:13:37 -0300

    Alteração nas nomenclaturas

--- MVC_2022/Models/Lanche.cs
+++ MVC_2022/Models/Lanche.cs
@@ -11,18 +11,18 @@ namespace LanchesMac.Models
         public int LancheId { get; set; }
 
         [Required(ErrorMessage = "O nome do lanche deve ser informado")]
-        [Display(Name = "Nome do Lanche")]
+        [Display(Name = "Nome do Produto")]
         [StringLength(80, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2} caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
-        [Display(Name = "Descrição do Lanche")]
+        [Display(Name = "Descrição")]
         [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
         [MaxLength(200, ErrorMessage = "Descrição pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
 
         [Required(ErrorMessage = "O descrição detalhada do lanche deve ser informada")]
-        [Display(Name = "Descrição detalhada do Lanche")]
+        [Display(Name = "Descrição detalhada")]
         [MinLength(20, ErrorMessage = "Descrição detalhada deve ter no mínimo {1} caracteres")]
         [MaxLength(200, ErrorMessage = "Descrição detalhada pode exceder {1} caracteres")]
         public string DescricaoDetalhada { get; set; }
